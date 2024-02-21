a5b4cfe63aaed5df334aa1b644b0860c9ae27955
1tapis <tapishsingh7191@gmail.com>
2024-02-21 17:14:17 +0530

    ArrayList of Data types

---
+++ question1.js
@@ -0,0 +1,4 @@
+// Create an array/list of names of all primitive data types in javascript.
+
+const primitiveDataType =["string","number","boolen","null","undefine","symbol","Bigint"];
+console.log(primitiveDataType);

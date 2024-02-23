4a2b4ebaadf5f2a15ca3168e3b2b4fcd979b2806
1tapis <tapishsingh7191@gmail.com>
2024-02-23 17:19:54 +0530

    loop problem

---
+++ question13.js
@@ -0,0 +1,10 @@
+function midleft(arr){
+    // if(arr.length %2 === 0){
+        for(let i=Math.round(arr.length/2); i<arr.length;i++){
+             console.log(arr[i])
+        }
+    // }
+
+}
+const arr= [1,2,3,4,5,6,7,8,9,10,11];
+midleft(arr);

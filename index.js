60f8f5f9226932f779a6ff0a1a636a5f03c73a04
1tapis <tapishsingh7191@gmail.com>
2024-04-02 16:14:52 +0530

    mm

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const button = document.getElementById("button");
 const ptag = document.getElementById("textLower");
 
 
-input.addEventListener('change',(e)=>{
-    let content = e.target.value;
+// input.addEventListener('change',(e)=>{
+//     let content = e.target.value;
 
-    button.addEventListener('click', (e) =>{
-        e.preventDefault();
-        ptag.innerHTML = 'Hi '+content+'!';
-        input.value="";
-    })
-})
+//     button.addEventListener('click', (e) =>{
+//         e.preventDefault();
+//         ptag.innerHTML = 'Hi '+content+'!';
+//         input.value="";
+//     })
+// })
+
+for(let i=0; i<ptag.length ;i++){
+    console.log(ptag[i].textContent);
+}
